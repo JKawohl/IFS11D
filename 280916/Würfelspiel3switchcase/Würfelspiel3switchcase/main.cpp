7dd4fdf703f81d839709e63453f90398d1a4253b
Kawohl <john@owncloud.com>
2016-09-28 16:52:15 +0200

    Stuff from school

--- 280916/Würfelspiel3switchcase/Würfelspiel3switchcase/main.cpp
+++ 280916/Würfelspiel3switchcase/Würfelspiel3switchcase/main.cpp
@@ -8,34 +8,39 @@
 using namespace std;
 
 int main()
-
 {
 	int aiZmZ[10] = { 0 };
+
 	int wieoft[7] = { 0 };
+	// randomzizer anwerfen 
 
 	srand((unsigned)time(NULL));
 
-	for ( int Wnr = 0; Wnr <= 9; Wnr++)
+	for (int Wnr = 0; Wnr <= 9; Wnr++)
 	{
-		aiZmZ[Wnr]= rand() %6 + 1;
+		// Würfeln
+		aiZmZ[Wnr] = rand() % 6 + 1;
 
 		wieoft[aiZmZ[Wnr]]++;
 
-		cout << aiZmZ[Wnr] << " ";	
+		cout << aiZmZ[Wnr] << " ";
 	}
+
 	// Zaehler zählt
 
 
-	cout << "\n";
+	/*cout << "\n";
 	for (int Zaehler = 1; Zaehler <= 6; Zaehler++)
 	{
 		cout << endl;
 		cout << " Zahl:" << Zaehler << " wurde " << wieoft[Zaehler] << " mal gewuerfelt" << endl;
-	 }
+	}*/
 
-	 
+
+
+
 	getchar();
 	return 0;
-	
+
 }
 
