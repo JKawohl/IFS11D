63b87d157ae9ac70b997eb48221348fdc9834367
Kawohl <john@owncloud.com>
2016-10-05 09:38:13 +0200

    Finished struct

--- 041016/StructWürfelspieler/StructWürfelspieler/main.cpp
+++ 041016/StructWürfelspieler/StructWürfelspieler/main.cpp
@@ -1,63 +1,89 @@
 // Würfelspiel_struct.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
 //
 
+
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
 #include <string>
 
 using namespace std;
+
 void haeufigkeit(unsigned int, unsigned int[]);
 
 int main()
 {
-	const int MAXS = 1;
-
-	struct T_Spieler { char Name[20]; unsigned int Wuerfel[10]; unsigned int igesamt; };
-	T_Spieler Spieler[8];
+	const unsigned int max = 8;
+	unsigned int groeßer = 0;
+	struct T_Spieler { string Name; unsigned int Wuerfel[10]; unsigned int gesamt; unsigned int statistik[6]; };
+	T_Spieler Spieler[max];
 	unsigned int haeufig[6] = { 0 };
 
-
-	//Spielernamen einlesen
-	cout << "Bitte geben Sie einen Namen ein: \n";
-	cin >> Spieler[0].Name;
+	for (int i = 0; i < max; i++)
+	{
+		cout << "Bitte geben Sie einen Namen ein: \n";
+		cin >> Spieler[i].Name;
+		Spieler[i].gesamt = 0;
+		for (int z = 0; z < 6; z++)
+		{
+			Spieler[i].statistik[z] = 0;
+		}
+	}
 	srand((unsigned)time(NULL));
 
-	
-
-	for (int i = 0; i < 10; i++)
+	for (int z = 0; z < max; z++)
 	{
-		int Zahl = 0;
-		Zahl = rand() % 6 + 1;
-		Spieler[0].Wuerfel[i] = Zahl;
-		haeufigkeit(Zahl, haeufig);
+		for (int i = 0; i < 10; i++)
+		{
+			int Zahl = 0;
+			Zahl = rand() % 6 + 1;
+			Spieler[z].Wuerfel[i] = Zahl;
+			haeufigkeit(Zahl, haeufig);
+			Spieler[z].gesamt = Spieler[z].gesamt + Spieler[z].Wuerfel[i];
+
+		}
 	}
 
-	// Zahlen des Spielers werden ausgegeben
-	cout << "Zahlen von: " << Spieler[0].Name << " ";
-	for (int i = 0; i < 10; i++)
+	for (int z = 0; z < max; z++)
 	{
-		cout << Spieler[0].Wuerfel[i] << " ";
+		cout << "Zahlen von " << Spieler[z].Name << " : ";
+		for (int i = 0; i < 10; i++)
+		{
+			cout << Spieler[z].Wuerfel[i] << " ";
+		}
+		cout << endl << endl;
 	}
 
+	for (int i = 0; i < max; i++)
+	{
+		cout << "Die maximale Augenzahl des Spielers: " << Spieler[i].Name << " : " << Spieler[i].gesamt << endl;
+	}
 	cout << endl << endl;
 
-
-	//Haeufigkeiten der Zahlen werden ausgegeben.
 	for (int i = 0; i < 6; i++)
 	{
-		cout << "Haeufigkeit der Zahl: " << i + 1 << " ist: " << haeufig[i] << endl;
+		cout << "Häufigkeit der Zahl: " << i + 1 << " ist: " << haeufig[i] << endl;
 	}
 
-	// die Konstante wird ausgegeben.
-	cout << MAXS;
+	//Sortieren
+	cout << "Sortiert nach maximaler Augenzahl: " << endl;
 
-	getchar();
-	getchar();
+	for (int i = 0; i < max - 1; i++)
+	{
+		if (Spieler[i].gesamt > Spieler[i + 1].gesamt)
+		{
+			groeßer = i;
+		}
+		else
+		{
+			groeßer = i + 1;
+		}
+	}
+	cout << "Die Augenzahl von " << Spieler[groeßer].Name << " ist größer!" << endl;
+
+	system("Pause");
 	return 0;
 }
-
-
 
 void haeufigkeit(unsigned int wuerfelzahl, unsigned int haeufig[]) {
 	switch (wuerfelzahl)
