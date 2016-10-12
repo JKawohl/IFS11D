05926ea7968dfeea320ccc16e3caa8618c220455
mueller-ma <you@example.com>
2016-10-12 16:04:08 +0200

    some little improvements

--- polarkart/polarkart/polarkart/main.cpp
+++ polarkart/polarkart/polarkart/main.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 
 using namespace std;
-#define M_PI 3.14159265
+//#define M_PI 3.14159265
 
 
 
@@ -12,8 +12,8 @@ struct T_kart {
 };
 
 struct T_polar {
-		float fWinkel;
-		float fRadius;
+	float fWinkel;
+	float fRadius;
 };
 
 T_polar kart_polar(T_kart KKoordi);
@@ -23,45 +23,57 @@ int main()
 {
 	T_kart KartKoordinaten;
 	T_polar Polarkoordinaten;
-	int i_Auswahl = 0;
+	int i_Auswahl = 1;
 
-	cout << "Bitte geben Sie 1 ein um die Winkelkoordinaten zu erfahren\noder 2 um die Radiuskoordinaten zu erfahren." << endl;
-	cin >> i_Auswahl;
+	while (i_Auswahl != 0) {
+		cout << "Bitte geben Sie 1 ein um die Winkelkoordinaten zu erfahren\noder 2 um die Radiuskoordinaten zu erfahren." << endl;
+		cin >> i_Auswahl;
 
-	switch (i_Auswahl) {
-	case 1: cout << " Winkelkoordinate: "; cin >> Polarkoordinaten.fWinkel;
-		cout << " Radiuskoordinate: "; cin >> Polarkoordinaten.fRadius;
-		KartKoordinaten = polar_kart(Polarkoordinaten);
-		cout << "X:  " << KartKoordinaten.fx << "\tY: " << KartKoordinaten.fy;
-		break;
+		switch (i_Auswahl) {
+		case 0:
+			return 0;
+		case 1:
+			cout << " Winkel: ";
+			cin >> Polarkoordinaten.fWinkel;
+			cout << " Radius: ";
+			cin >> Polarkoordinaten.fRadius;
+			KartKoordinaten = polar_kart(Polarkoordinaten);
+			cout << "X:  " << KartKoordinaten.fx << "\tY: " << KartKoordinaten.fy;
+			break;
 
-	case 2: cout << "X Koordinate: "; cin >> KartKoordinaten.fx;
-		cout << "Y Koordinate: "; cin >> KartKoordinaten.fy;
+		case 2:
+			cout << "X Koordinate: ";
+			cin >> KartKoordinaten.fx;
+			cout << "Y Koordinate: ";
+			cin >> KartKoordinaten.fy;
 
-		Polarkoordinaten = kart_polar(KartKoordinaten);
-		cout << "fWinkel: " << Polarkoordinaten.fWinkel << " \tfRadius: "
-			<< Polarkoordinaten.fRadius;
-		break;
-	default: cout << "Bitte treffen Sie eine gültige Auswahl";
-	}
-	getchar();
-	getchar();
-		return 0;
-
-}
-
-		T_polar kart_polar(T_kart KKoordi)
-		{
-			T_polar PKoordi;
-			PKoordi.fWinkel = 180 / M_PI * tan (KKoordi.fy / KKoordi.fx);
-			PKoordi.fRadius = sqrt(KKoordi.fx * KKoordi.fx + KKoordi.fy * KKoordi.fy);
-			return (PKoordi);
-}
-		T_kart polar_kart(T_polar PKoordi)
-		{
-			T_kart KKoordi;
-			KKoordi.fx = PKoordi.fRadius * cos(PKoordi.fWinkel * M_PI / 180);
-			KKoordi.fy = PKoordi.fRadius * sin(PKoordi.fWinkel *  M_PI / 180);
-			return (KKoordi);
-
+			Polarkoordinaten = kart_polar(KartKoordinaten);
+			cout << "fWinkel: " << Polarkoordinaten.fWinkel << " \tfRadius: "
+				 << Polarkoordinaten.fRadius;
+			break;
+		default:
+			cout << "Bitte treffen Sie eine gültige Auswahl";
 		}
+		getchar();
+		getchar();
+	}
+	return 0;
+
+}
+
+T_polar kart_polar(T_kart KKoordi)
+{
+	T_polar PKoordi;
+	PKoordi.fWinkel = 180 / M_PI * tan (KKoordi.fy / KKoordi.fx);
+	PKoordi.fRadius = sqrt(KKoordi.fx * KKoordi.fx + KKoordi.fy * KKoordi.fy);
+	return (PKoordi);
+}
+
+T_kart polar_kart(T_polar PKoordi)
+{
+	T_kart KKoordi;
+	KKoordi.fx = PKoordi.fRadius * cos(PKoordi.fWinkel * M_PI / 180);
+	KKoordi.fy = PKoordi.fRadius * sin(PKoordi.fWinkel *  M_PI / 180);
+	return (KKoordi);
+
+}
