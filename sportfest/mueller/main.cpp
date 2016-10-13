bd741996ccdc31f6bcf76a7c6b0d8a8a07b7152a
mueller-ma <mueller-ma@users.noreply.github.com>
2016-10-13 12:14:31 +0200

    updated to latest version

--- sportfest/mueller/main.cpp
+++ sportfest/mueller/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 
-const int MAX_SPIELER = 10;
+#define ANZAHL_WEITSPRUNG	3
+#define ANZAHL_KUGELSTOSSEN	6
+#define ANZAHL_SPEERWERFEN	6
+const int MAX_SPIELER = 4;
 
 int main() {
 	using namespace std;
 	struct T_DISZIPLINEN {
-		double weitsprung[3] = {0, 0, 0};
-		double kugelstossen[6] = {0, 0, 0, 0, 0, 0};
-		double speerwerfen[6] = {0, 0, 0, 0, 0, 0};
+		double weitsprung[ANZAHL_WEITSPRUNG];
+		int weitesterWeitsprung = -2;
+		double kugelstossen[ANZAHL_KUGELSTOSSEN];
+		double speerwerfen[ANZAHL_SPEERWERFEN];
 		double hundert_m_lauf = 0;
 	};
 	struct T_SPORTLER {
@@ -24,7 +28,7 @@ int main() {
 	cout << "=======================================================" << endl << endl;
 
 	for(int aktuellerSpieler = 0; aktuellerSpieler < MAX_SPIELER; aktuellerSpieler++) {
-		cout << "Spieler " << aktuellerSpieler + 1 << "\nName: ";
+		cout << "Sportler " << aktuellerSpieler + 1 << "\nName: ";
 		cin >> athleten[aktuellerSpieler].name;
 		cout << "Vorname: ";
 		cin >> athleten[aktuellerSpieler].vorname;
@@ -32,10 +36,28 @@ int main() {
 		cin >> athleten[aktuellerSpieler].alter;
 	}
 
-	for(int aktuellerWurf = 0; aktuellerWurf < 4; aktuellerWurf++) {
+	cout << endl;
+
+	for(int aktuellerSprung = 0; aktuellerSprung < ANZAHL_WEITSPRUNG; aktuellerSprung++) {
+		cout << "Sprung " << aktuellerSprung + 1 << endl;
 		for(int aktuellerSpieler = 0; aktuellerSpieler < MAX_SPIELER; aktuellerSpieler++) {
-			cout << "Wurf " << aktuellerWurf + 1 << "\nSpieler " << aktuellerSpieler + 1 << ": ";
-			cin >> athleten[aktuellerSpieler].disziplinen.weitsprung[aktuellerWurf];
+			cout << aktuellerSpieler + 1 << ": " << athleten[aktuellerSpieler].name << ", " << athleten[aktuellerSpieler].vorname << ": ";
+			cin >> athleten[aktuellerSpieler].disziplinen.weitsprung[aktuellerSprung];
+			if(athleten[aktuellerSpieler].disziplinen.weitsprung[athleten[aktuellerSpieler].disziplinen.weitesterWeitsprung] < athleten[aktuellerSpieler].disziplinen.weitsprung[aktuellerSprung]) {
+				athleten[aktuellerSpieler].disziplinen.weitesterWeitsprung = aktuellerSprung;
+			}
 		}
 	}
+
+	cout << "\nBestleistungen Weitsprung:\n";
+	int gewinnerWeitsprung = 0;
+	for(int aktuellerSpieler = 0; aktuellerSpieler < MAX_SPIELER; aktuellerSpieler++) {
+		cout << "Die Bestweite von " << athleten[aktuellerSpieler].name << ", " << athleten[aktuellerSpieler].vorname << " betrug " << athleten[aktuellerSpieler].disziplinen.weitsprung[athleten[aktuellerSpieler].disziplinen.weitesterWeitsprung] << "m\n";
+		if(athleten[gewinnerWeitsprung].disziplinen.weitsprung[athleten[gewinnerWeitsprung].disziplinen.weitesterWeitsprung] < athleten[aktuellerSpieler].disziplinen.weitsprung[athleten[aktuellerSpieler].disziplinen.weitesterWeitsprung]) {
+			athleten[gewinnerWeitsprung].disziplinen.weitsprung[athleten[gewinnerWeitsprung].disziplinen.weitesterWeitsprung] = athleten[aktuellerSpieler].disziplinen.weitsprung[athleten[aktuellerSpieler].disziplinen.weitesterWeitsprung];
+		}
+	}
+//FIXME Spieler 0 gewinnt immer
+	cout << "\nGewinner Weitsprung: " << athleten[gewinnerWeitsprung].name << ", " << athleten[gewinnerWeitsprung].vorname << endl;
+	return 0;
 }
