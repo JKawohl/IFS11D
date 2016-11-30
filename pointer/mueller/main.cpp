230191e4abdbd823f3ecc4ad9fd37100cdb83c72
mueller-ma <mueller-ma@users.noreply.github.com>
2016-11-30 10:24:12 +0100

    updated pointer

--- pointer/mueller/main.cpp
+++ pointer/mueller/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void fuellen(char * p_char, int * p_int);
+void fuellen2(char * p_char, int * p_int);
 void showArray(int * p_int);
 
 int main() {
@@ -8,15 +9,12 @@ int main() {
 	int aiFeld[20] = {0};
 	char acZeichen[21];
 	cout << "Zeichen eingeben (max. 20 Zeichen)\n";
-	cin >> acZeichen;
+	cin.getline(acZeichen, 21);
 
-	fuellen(acZeichen, aiFeld);
-	cout << "Int Array:\n";
-	//for(int i = 0; i < 20; i++) {
-	//cout << aiFeld[i] << " \n";
-	//}
+	//fuellen(acZeichen, aiFeld);
+	fuellen2(acZeichen, aiFeld);
+
 	showArray(aiFeld);
-
 }
 
 void fuellen(char * p_char, int * p_int) {
@@ -25,9 +23,15 @@ void fuellen(char * p_char, int * p_int) {
 	}
 }
 
+void fuellen2(char p_char[], int p_int[]) {
+	for(int i = 0; p_char[i] != '\0'; i++) {
+		p_int[i] = p_char[i];
+	}
+}
+
 void showArray(int * p_int) {
 	for(int i = 0; * (p_int + i) != 0; i++) {
-		std::cout << *(p_int + i) << "\n";
+		std::cout << i+1 << ":\t" << (char) *(p_int + i) << " = " << *(p_int + i) << "\n";
 	}
 }
 
