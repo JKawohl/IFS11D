a1517e84fd54d43f50a434097d9dbfa7901cda58
Kawohl <john@owncloud.com>
2017-03-28 08:39:33 +0200

    added stuff

--- php/formulare/kaese.php
+++ php/formulare/kaese.php
@@ -1,11 +1,17 @@
 <?php
-var_dump($_POST);
-if(trim)$_POST["txtName"])== ""){
-	echo "Bitte geben Sie Ihren Namen ein";
-	echo "<p> <a href=\"kaeseformular.html\"> zurück </a>";}
-}
-echo "Name"
-	
 
+if(trim($_POST["txtName"])== "") 
+{
+	echo "Bitte geben Sie Ihren Namen ein";
+	echo "<p> <a href=\"kaeseformular.html\"> zurück </a>";
+}
+   echo "Name: ".$_POST["txtName"]."<br>";
+if(isset($_POST["chxCamenbert"])) echo "Ihr gewaehlter Kaese : Camenbert "; "<br>";	
+if(isset($_POST["chxEmmentaler"])) echo "Ihr gewaehlter Kaese : Emmentaler "; "<br>";	
+if(isset($_POST["chxEdamer"])) echo "Ihr gewaehlter Kaese : Edamer "; "<br>";	
+if(isset($_POST["chxKraversal"])) echo "Ihr gewaehlter Kaese : Kraversal "; "<br>";	
+
+echo "Wunsch: ".$_POST ["txtWunsch"]."<br>";
+echo "Ihr Zahlungswunsch :" .$_POST ["optBezahlung"]. "<br>";
 
 ?>
